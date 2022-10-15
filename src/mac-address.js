5c6070905154c8951a33d6bb3166edc6b5ec510e
aleksryab <aleksryabtsev@gmail.comm>
2022-10-15 15:56:20 +0700

    done: MAC Adress

--- src/mac-address.js
+++ src/mac-address.js
@@ -14,10 +14,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function isMAC48Address(n) {
+  if (typeof n !== 'string') return;
+  const regex = /^([\dA-F]{2}-){5}([\dA-F]{2})$/i;
+  return !n.search(regex);
 }
+
 module.exports = {
   isMAC48Address
 };
